da74328b9f8f44797657423aff52859db5a2d45e
beem_app <phingocthang@yahoo.com.vn>
2014-09-26 18:16:57 +0700

    be em app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeemApp::Application.config.secret_token = '47ad6cc9ba8e06c8229ec768eaadca7b93d274c0e7c64bb57e90f3e3d52a95de305c626de54b9a595225adacd293ced2077a288c11e1eeedd396e83cc2cee520'
